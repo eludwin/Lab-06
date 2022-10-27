78dc7213ffdf9c704004b6b2fd5a35b75d3f92cc
Elli Ludwin <e.ludwin@ufl.edu>
2022-10-27 00:44:02 -0400

    Edit lab-06.py

--- lab-06.py
+++ lab-06.py
@@ -1,8 +1,12 @@
 def sum(num1, num2):
     sum = num1 + num2
     return sum
+def sub(num1, num2):
+    sub = num1 - num2
+    return sub
 
 num1 = 5
 num2 = 8
 
 print(sum(num1, num2))
+print(sub(num1, num2))
